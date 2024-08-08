7e36eebdc12316087c1743e908d38eff158048d2
Etienne Koekemoer <me@etiennek.com>
2024-08-08 18:21:08 +0200

    Added daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,31 @@
 import { type Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  plugins: [
+    typography,
+    // deno-lint-ignore no-explicit-any
+    daisyui as any,
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+        },
+      },
+    ],
+  },
 } satisfies Config;
